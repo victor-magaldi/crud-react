b69702abfeb06a166fe4f0fda3f30930f48b96a2
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-04-04 16:47:22 -0300

    customizando footer

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <span>
+        Desenvolvido Por Victor de Souza Magaldi{" "}
+        <i className="fa fa-heart text-danger"></i>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
