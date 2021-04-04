6a65745c0b075ca773074d3c012aeea22f9e322a
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-04-04 16:02:36 -0300

    customiza estilo logo react

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Logo.css";
 
 const Logo = () => {
-  return <aside className="logo">Logo</aside>;
+  return (
+    <aside className="logo">
+      <a href="/">
+        <h3>CRUDREACT</h3>
+      </a>
+    </aside>
+  );
 };
 
 export default Logo;
