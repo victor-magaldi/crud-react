655eaffc6018157ff9e7d85a8c2f4d6d18e1e6d4
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-04-05 19:03:48 -0300

    estiliza navegação

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => {
-  return <nav className="navegacao">Nav</nav>;
+  return (
+    <aside className="menu-area">
+      <nav className="menu">
+        <a href="/#">
+          <i className="fa fa-home"></i>Inicio
+        </a>
+        <a href="/#">
+          <i className="fa fa-users"></i>Usuário
+        </a>
+      </nav>
+    </aside>
+  );
 };
 
 export default Nav;
