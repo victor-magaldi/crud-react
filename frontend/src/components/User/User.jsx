85826dd0a0a39e7e36fc74168d066baa8191cd78
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-04-08 20:49:36 -0300

    inicia cadastro de usuário

--- frontend/src/components/User/User.jsx
+++ frontend/src/components/User/User.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Main from "../templates/Main";
 
-// const headerProps = {
-//   icon: "user",
-//   title: "usuários",
-//   subtitle: "cadastro de usuários",
-// };
-const User = () => {
+const headerProps = {
+  icon: "user",
+  title: "usuários",
+  subtitle: "cadastro de usuários",
+};
+const baseUrl = "http://localhost:3001/users";
+const initialState = { user: { name: "", email: "" }, list: [] };
+
+function User() {
+  const [objUser, setUser] = useState(initialState);
   React.useEffect(() => {
     // Your code here
   }, []);
-  return (
-    <Main icon="home" title="inicio" subtitle="Desenvolvimento Crud React ">
-      <div className="display-1">Cadastro!</div>
-      <hr />
-      <p className="mb-0">
-        Sistema para exemplificar a Construção de um cadastro em react
-      </p>
-    </Main>
-  );
-};
+  function clear() {
+    setUser(initialState);
+  }
+  function save() {
+    const user = objUser.user;
+    const method = user.id ? "put" : "post";
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+
+    function getUpdateList(user) {
+      const list = objUser.list.filter((item) => item.id !== user.id);
+      list.unshift(user);
+      return list;
+    }
+    axios[method](url, user).then((resp) => {
+      const list = getUpdateList(resp.data);
+      const stateUser = { user: user, list: list };
+      setUser(stateUser);
+    });
+  }
+  return <Main {...headerProps}>Cadastro de Usuário</Main>;
+}
 
 export default User;
